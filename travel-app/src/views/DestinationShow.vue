73f53c0ca39670ce994db01832d6b3204e20d8f9
lvl12sealclubber <maxpower713@gmail.com>
2022-01-31 14:15:06 -0500

    Test data

--- travel-app/src/views/DestinationShow.vue
+++ travel-app/src/views/DestinationShow.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
-    <div v-if="destination" class="destination">
+    <div v-if="!destination.video" class="destination">
       <h1>{{ destination.name }}</h1>
-      <GoBack />
+      <!-- <GoBack /> -->
       <div class="destination-details">
         <img :src="`/images/${destination.image}`" :alt="destination.name" />
         <p>{{ destination.description }}</p>
         <p v-if="destination.test">{{ destination.test }}</p>
       </div>
     </div>
+    <div v-if="destination.video" class="video-embed">
+      <h1>{{ destination.name }}</h1>
+
+      <iframe
+        width="100%"
+        height="400"
+        :src="`https://www.youtube-nocookie.com/embed/${destination.video}`"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+
     <div class="experiences">
-      <h2>Top Expereinces in {{ destination.name }}</h2>
+      <h2>Photos of {{ destination.name }}</h2>
       <div class="cards">
         <router-link
           v-for="experience in destination.experiences"
@@ -29,10 +43,10 @@
 </template>
 <script>
 import sourceData from "@/data.json";
-import GoBack from "@/components/GoBack.vue";
+// import GoBack from "@/components/GoBack.vue";
 import ExperienceCard from "@/components/ExperienceCard.vue";
 export default {
-  components: { ExperienceCard, GoBack },
+  components: { ExperienceCard },
   props: {
     id: { type: Number, required: true },
   },
@@ -48,3 +62,8 @@ export default {
 // Load data via fetch from external api
 // https://vueschool.io/lessons/reacting-to-param-changes
 </script>
+<style scoped>
+.video-embed {
+  margin-top: 40px;
+}
+</style>
